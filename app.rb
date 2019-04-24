3df6d60ea62b84a458a18dc341d8d5c29aebbb83
A Hashim <ahashim983@yahoo.com>
2019-04-24 15:38:31 +0100

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    #gets and displays form template
+    erb :newteam
+  end
+
+  post '/team' do
+    #displays data from completed form
+    erb :team
+  end
+
 
 end
